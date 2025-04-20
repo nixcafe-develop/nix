3df388c498dd218ef84cdc63fd2733d3d90f56c6
sobte <i@sobte.me>
2025-04-21 07:37:39 +0800

    feat: add patchelf

--- shells/default/default.nix
+++ shells/default/default.nix
@@ -11,6 +11,8 @@ mkShell {
     nixfmt-rfc-style
     deadnix
     statix
+    # PatchELF is a simple utility for modifying existing ELF executables and libraries.
+    # patchelf
   ];
 
   inherit (inputs.self.checks.${system}.pre-commit-check) shellHook;
