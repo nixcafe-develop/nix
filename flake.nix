0c217369afd7ad9725e30d74798da58933bcb38c
sobte <i@sobte.me>
2025-05-20 21:39:21 +0800

    feat: use flakehub to avoid github api limit

--- flake.nix
+++ flake.nix
@@ -1,14 +1,16 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # nixos-unstable (use flakehub to avoid github api limit)
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
 
     snowfall-lib = {
-      url = "github:snowfallorg/lib";
+      url = "https://flakehub.com/f/snowfallorg/lib/*.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # see: https://github.com/cachix/git-hooks.nix
     pre-commit-hooks = {
-      url = "github:cachix/git-hooks.nix";
+      url = "https://flakehub.com/f/cachix/git-hooks.nix/0.1.*.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
